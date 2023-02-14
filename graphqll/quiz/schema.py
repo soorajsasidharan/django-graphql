d584ec9ae3ed5be47d3f7490a746ab81c474bcf2
sooraj <sooraj@gamil.com>
2023-02-14 09:41:12 +0530

    first commit

---
+++ graphqll/quiz/schema.py
@@ -0,0 +1,71 @@
+import graphene
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import Category, Question, Quizzes, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id","name")
+
+class QuizzersType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id","title","category","quiz")
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title","quiz")
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question","answer_text")
+
+class Query(graphene.ObjectType):
+
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+    all_answers = graphene.List(AnswerType, id=graphene.Int())
+
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.filter(question=id)
+
+# class CategoryMutation(graphene.Mutation):
+    
+#     class Arguments:
+#         name = graphene.String(required=True)
+    
+#     category = graphene.Field(CategoryType)
+    
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
+
+class CategoryMutation(graphene.Mutation):
+
+    class Arguments:
+
+        id = graphene.ID()
+        name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+    @classmethod
+    def mutate(cls, root, info, name,id):
+        category = Category.objects.get(id=id)
+        category.name = name
+        category.save()
+        return CategoryMutation(category=category)
+
+
+    
+
+class Mutation(graphene.ObjectType):
+
+    update_category = CategoryMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
